5070e69236de744b8a07735699c689af4635d872
takumi-333 <takumi_soccer0514@icloud.com>
2023-09-30 17:54:48 +0900

    [add] GUIの追加

--- sample.py
+++ sample.py
@@ -1,36 +1,60 @@
-from enum import Enum
 import PySimpleGUI as sg
 
-# def culCalorie(weight, height, old):
-#     calorie = ((0.0481 * weight + 0.0234 * height - 0.0138 * old - 0.4235) * 1000 ) / 4186
-#     return calorie
+def signUpPage():
+    frame1 = sg.Frame('',
+        [
+            # テキストレイアウト
+            [
+                sg.Text('体重を入力してください', font=('メイリオ',12))
+            ],
+            #画像選択ボタン ※3つカンマ区切りで書いてるのでこれらが同じ行に配置される
+            [
+                sg.Text("体重"),
+                sg.InputText('', key='-INPUTTEXT-', enable_events=True,), 
+                sg.Text("kg")
+            ],
+            #OCR開始ボタン
+            [
+                sg.Submit(button_text='消費カロリー計算開始',
+                        font=('メイリオ',8),size=(8,3),key='calorie')
+            ]
+        ], size=(500, 700)
+    )
+    layout =  [[frame1]]
+    
+    return sg.Window("情報登録", layout, finalize=True)
 
 def main():
-    # weight = input("体重を入力してください")
-    # height = input("身長を入力してください")
-    # old = input("年齢を入力してください")
-    # print("身長 = " + str(height) + "cm")
-    
-    sg.theme("DarkBlue")
 
-    layout=[[sg.Text("これはPySimpleGUIのテストです。")],
-            [sg.Listbox(values=sg.theme_list(), size=(20, 12), key='-LIST-', enable_events=True)]]
+    #先程確認して決めたテーマカラーを設定
+    sg.theme('Purple')
 
-    window=sg.Window("test",layout)
+    """
+    ・sg.Imageで画像部品をのせられる
+    ・sg.Textでテキスト部品をのせられる
+    ・sg.InputTextでテキスト入力エリアをのせる(画像Pathを表示させる部分)
+    ・sg.FileBrowseでWindowsでよく見るファイル選択画面を出せる(InputTextの横に置けばtextを自動入力してくれる)
+    ・sg.Submitはいわゆる「決定ボタン」。今回はOCR開始ボタンとして使った
+    ・sg.MLineはテキスト出力エリア
+    ・keyは、後でイベントを追加する時に参照する変数名
+    ・後は省略できるが、サイズやフォントも各命令で指定出来る
+    """
 
+    #GUIタイトルと全体レイアウトをのせたWindowを定義する
+    window = signUpPage()
+
+    #GUI表示実行部分
     while True:
-        event,values=window.read()
-        if event == sg.WIN_CLOSED:
+        # ウィンドウ表示
+        event, values = window.read()
+
+        #クローズボタンの処理
+        if event is None:
+            print('exit')
             break
 
-    #画面を閉じます。
     window.close()
 
-    # gender = Gender.Male
-    # if gender == Gender.Male:
-    #     print("あなたは男性です")
-    # else:
-    #     print("あなたは女性です")
 
 if __name__ == "__main__":
     main()
