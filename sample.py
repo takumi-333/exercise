ad8f8811e86f8d1571bc48ac6209d1cc2c54afc1
takumi-333 <takumi_soccer0514@icloud.com>
2023-09-30 16:37:51 +0900

    [add]身長と体重を入力できるようになりました

--- sample.py
+++ sample.py
@@ -7,8 +7,17 @@ class Gender(Enum):
 def sum(a, b)->int:
     return a+b
 
+# def culCalorie(weight, height, old):
+#     calorie = ((0.0481 * weight + 0.0234 * height - 0.0138 * old - 0.4235) * 1000 ) / 4186
+#     return calorie
 
 def main():
+    weight = input("体重を入力してください")
+    height = input("身長を入力してください")
+    old = input("年齢を入力してください")
+
+    print("身長 = " + str(height) + "cm")
+    
     gender = Gender.Male
     if gender == Gender.Male:
         print("あなたは男性です")
